21913a26bde205091aa60d38ea4ba984c5a482c2
Guillaume Aubert <guillaume@aubert.ws>
2025-03-18 21:16:16 +0100

    fix!: clean code and remove deprecated routes

--- src/index.js
+++ src/index.js
@@ -12,26 +12,6 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/index.html'));
 })
 
-app.get('/translations', (req, res) => {
-    const currentLocale = req.getLocale();
-    const translations = require(`../assets/locales/${currentLocale}.json`);
-    res.json(translations);
-});
-
-app.get('/translations/all', (req, res) => {
-    const translations = {};
-
-    // Read each translation file for all languages
-    i18n.getLocales().forEach(locale => {
-        const filePath = path.join(__dirname, '../assets/locales', `${locale}.json`);
-        if (fs.existsSync(filePath)) {
-            translations[locale] = require(filePath);
-        }
-    });
-
-    res.json(translations);
-});
-
 app.listen(port, () => {
     console.log(`Example app listening on http://127.0.0.1:${port}`)
 })
