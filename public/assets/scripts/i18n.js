a81c350e9592687b75d504f32c6d8e7ad9d5e631
Guillaume Aubert <guillaume@aubert.ws>
2025-03-26 10:49:30 +0100

    feat: add javascript for password validation

--- public/assets/scripts/i18n.js
+++ public/assets/scripts/i18n.js
@@ -20,7 +20,15 @@ function initI18n() {
                         loginFormTitle: "Log In",
                         loginFormEmailTooltip: "Enter email",
                         loginFormpassword: "Enter password",
-                        loginPageSubtitle: "Please log in before accessing your tierlists"
+                        loginPageSubtitle: "Please log in before accessing your tierlists",
+                        error: {
+                            password_special: "Password need to contain at least a special character",
+                            password_digit: "Password need to contain at least a number",
+                            password_lowercase: "Password need to contain at least a lowercase letter",
+                            password_uppercase: "Password need to contain at least an uppercase letter",
+                            password_length: "Password need to at least 8 characters long",
+                            email_invalid: "Your email is invalid",
+                        }
                     },
                 },
                 fr: {
@@ -32,7 +40,15 @@ function initI18n() {
                         loginFormTitle: "Formulaire de connection",
                         loginFormEmailTooltip: "e-mail",
                         loginFormpassword: "mot de passe",
-                        loginPageSubtitle: "Merci de vous connecter avant d'accéder à vos tierlists"
+                        loginPageSubtitle: "Merci de vous connecter avant d'accéder à vos tierlists",
+                        error: {
+                            password_special: "Votre mot de passe doit contenir des characteres speciaux",
+                            password_digit: "Votre mot de passe doit contenir au moins 1 nombre",
+                            password_lowercase: "Votre mot de passe doit contenir au moins une lettre minuscule",
+                            password_uppercase: "Votre mot de passe doit contenir au moins une lettre majuscule",
+                            password_length: "Votre mot de passe doit faire au moins 8 characteres de long",
+                            email_invalid: "Votre email est invalide",
+                        }
                     },
                 },
                 jp: {
@@ -44,7 +60,15 @@ function initI18n() {
                         loginFormTitle: "ログイン",
                         loginFormEmailTooltip: "電子メール",
                         loginFormpassword: "パスワード",
-                        loginPageSubtitle: "ティアリストにアクセスする前にログインしてください。"
+                        loginPageSubtitle: "ティアリストにアクセスする前にログインしてください。",
+                        error: {
+                            password_special: "パスワードには特殊文字を含める必要があります。",
+                            password_digit: "パスワードには少なくとも1つの数字が含まれていなければなりません。",
+                            password_lowercase: "パスワードには、少なくとも1つの小文字を含める必要があります。",
+                            password_uppercase: "パスワードには、少なくとも1つの大文字を含める必要があります。",
+                            password_length: "パスワードは8文字以上でなければなりません",
+                            email_invalid: "電子メールが無効です",
+                        }
                     },
                 },
             },
